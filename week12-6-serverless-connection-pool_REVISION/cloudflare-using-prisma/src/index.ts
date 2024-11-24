095f00153da63ddd7717bfdb53f256eb3da2b167
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-11-24 15:53:42 +0530

    Serverless using PRISMA and CLOUDFLARE workers REVISION

---
+++ week12-6-serverless-connection-pool_REVISION/cloudflare-using-prisma/src/index.ts
@@ -0,0 +1,66 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+// -------------------------------------------------------------------------------------------
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+export interface Env {
+	DATABASE_URL: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_URL,
+		}).$extends(withAccelerate());
+
+		const res = await prisma.log.create({
+			data: {
+				level: 'info',
+				message: 'abcd',
+				meta: { headers: JSON.stringify(request.headers) },
+			},
+		});
+
+		console.log('RESPONSE : ', res);
+		// return Response.json(JSON.stringify(res));
+
+		const resGot = await prisma.log
+			.findMany({
+				take: 20,
+				orderBy: {
+					id: 'desc',
+				},
+			})
+			.withAccelerateInfo();
+
+		console.log(`resGot : `, JSON.stringify(resGot));
+
+		// return Response.json(resGot);
+
+		const deleteUser = await prisma.log.deleteMany({
+			where: {
+			  email: 'bert@prisma.io',
+			},
+		  })
+	},
+};
+
+const prisma = new PrismaClient().$extends(withAccelerate());
